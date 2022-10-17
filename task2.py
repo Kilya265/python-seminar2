63965f1f91762e31aa96386951ae6bf47c7f2d93
Kilya265 <liliya.nikodimova@yandex.ru>
2022-10-17 21:56:08 +0700

    task2

---
+++ task2.py
@@ -0,0 +1,8 @@
+# Напишите программу, которая принимает на вход число N 
+# и выдает набор произведений чисел от 1 до N.
+
+num = int(input("Введите число: "))
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+    print('result: ', factorial)
